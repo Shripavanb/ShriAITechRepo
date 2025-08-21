58f6651baab32c7f677cc8bddd96ffebf33a7338
ShriGit01 <shripavanb@gmail.com>
2025-08-21 21:29:32 +0530

    First commit-Full Migrating from old Account to Shripavanb

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ShriAITech.Models;
+
+namespace ShriAITech.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult WebDevelopment()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Views/Home/WebDevelopment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShriAITech.Views.Home
+{
+    public class Web_DevelopmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
